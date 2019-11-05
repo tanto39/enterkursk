70899e6a5de468e8e30a4d3886d787d0e4f21609
enterkursk <tanto3@hornet.beget.ru>
2019-11-05 11:01:50 +0300

    fix leftmenu

--- templates/enterkursk/js/myscripts.js
+++ templates/enterkursk/js/myscripts.js
@@ -121,7 +121,7 @@ function scrollLeft(){
 	var fixLeft = $('.leftmenu');
 	var fixWidth = leftSidebar.width();
 	var heightToLeft = $('.header').height() + $('.topmenu').height();
-    var heightToFooter = $('.middle').height() - $('.footer').height() + 160;
+    var heightToFooter = $('.middle').height() - $('.footer').height() + $('.scroll').height()*1.6;
 
 	if (($(window).scrollTop() > heightToLeft) && ($(window).scrollTop() < heightToFooter))
         fixLeft.css({'position':'fixed','top':'15px','bottom':'auto'}).css('width', fixWidth - 2);
